6b0724f6261421f0504abbb74888ea3e50262c81
Jonathan Echeverria Armesto <jonathan.eche@gmail.com>
2025-11-29 15:49:27 +0100

    Adding more resources, coal, iron. Updating ui controls

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,5 +1,5 @@
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
-import { blocks } from './blocks';
+import {  resources } from './blocks';
 
 export function createUI(world) {
     const gui = new GUI();
@@ -12,13 +12,15 @@ export function createUI(world) {
     terrainFolder.add(world.params.terrain, 'magnitude', 0, 1).name('Magnitude');
     terrainFolder.add(world.params.terrain, 'offset', 0, 1).name('Offset');
 
-    const resourcesFolder = gui.addFolder('Resources');
-    resourcesFolder.add(blocks.stone, 'scarcity', 0, 1).name('Scarcity');
-
-    const scaleFolder = gui.addFolder('Scale');
-    scaleFolder.add(blocks.stone.scale, 'x', 10, 100).name('X Scale');
-    scaleFolder.add(blocks.stone.scale, 'y', 10, 100).name('Y Scale');
-    scaleFolder.add(blocks.stone.scale, 'z', 10, 100).name('Z Scale');
+    resources.forEach(resource => {
+        const resourcesFolder = gui.addFolder(resource.name);
+        resourcesFolder.add(resource, 'scarcity', 0, 1).name('Scarcity');
+    
+        const scaleFolder = resourcesFolder.addFolder('Scale');
+        scaleFolder.add(resource.scale, 'x', 10, 100).name('X Scale');
+        scaleFolder.add(resource.scale, 'y', 10, 100).name('Y Scale');
+        scaleFolder.add(resource.scale, 'z', 10, 100).name('Z Scale');
+    });
 
     gui.onChange(() => {
         world.generate();

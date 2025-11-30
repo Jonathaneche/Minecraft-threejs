78db15cc7cc47c94ab02d7241956746848440eec
Jonathan Echeverria Armesto <jonathan.eche@gmail.com>
2025-11-30 10:13:06 +0100

    Adding Player's  cameraHelper, and updating UI controls - checkbox to show/hide Player's camera helper

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -7,15 +7,18 @@ export function createUI(world, player) {
 
     const playerFolder = gui.addFolder('Player');
     playerFolder.add(player, 'maxSpeed', 1, 20).step(1).name('Max Speed');
+    playerFolder.add(player.cameraHelper, 'visible').name('Show Camera Helper');
 
-    gui.add(world.size, "width", 8, 128, 1).name("Width");
-    gui.add(world.size, "height", 8, 64, 1).name("Height");
     const terrainFolder = gui.addFolder('Terrain');
     terrainFolder.add(world.params, 'seed', 0, 10000).name('Seed');
     terrainFolder.add(world.params.terrain, 'scale', 10, 100).name('Scale');
     terrainFolder.add(world.params.terrain, 'magnitude', 0, 1).name('Magnitude');
     terrainFolder.add(world.params.terrain, 'offset', 0, 1).name('Offset');
+    
+    gui.add(world.size, "width", 8, 128, 1).name("Width");
+    gui.add(world.size, "height", 8, 64, 1).name("Height");
 
+    
     resources.forEach(resource => {
         const resourcesFolder = gui.addFolder(resource.name);
         resourcesFolder.add(resource, 'scarcity', 0, 1).name('Scarcity');
